55a75e9eedb5f2f1696cd32b8d7ffac42b7a53ad
wrewsama <andrewlzs512@gmail.com>
2023-03-11 19:15:35 +0800

    add create session form

--- frontend/src/components/create-session.tsx
+++ frontend/src/components/create-session.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+
 export default function CreateSession() {
+	const [newName, setNewName] = useState('')
+
+	const handleNameChange = event => {
+		setNewName(event.target.value)
+	}
+
+	const handleClick = event => {
+		console.log(newName)
+	}
 	return (
-		<div className="container bg-dark">
-			Create Session
+		<div className="container bg-dark text-light">
+			<div className="row text-center mb-5">
+				<h2> Create a new Session </h2>
+			</div>
+			<form>	
+				<div className="mb-3">
+					<label className="form-label"> Your Name </label>
+					<input type="text"
+					       className="form-control"
+						   value={newName}
+						   onChange={handleNameChange} />
+				</div>
+				<button className='btn btn-light'
+						onClick={handleClick}>
+					Create
+				</button>
+			</form>
 		</div>
 	)
 }
