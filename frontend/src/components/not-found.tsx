0b1276ca9029ffa5025c8344532245bb63cfdf4f
wrewsama <andrewlzs512@gmail.com>
2023-03-12 21:26:51 +0800

    handle generate random without suggestions

--- frontend/src/components/not-found.tsx
+++ frontend/src/components/not-found.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function NotFound() {
 	return (
-		<div className="d-flex align-items-center justify-content-center text-white">
+		<div className="d-flex align-items-center justify-content-center">
 			<div className="text-center">
 				<h1 className="display-1 fw-bold">404 </h1>
 				<p className="fs-3">
