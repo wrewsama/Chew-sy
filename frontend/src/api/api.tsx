f821ced4fb01595e4335b705aba646d0ac8eb1b4
wrewsama <andrewlzs512@gmail.com>
2023-03-16 16:18:35 +0800

    fix bugs

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-	baseURL: "http://localhost:9000",
+	baseURL: "https://chewsy-backend.onrender.com",
 	headers: {
 		"Content-type": "application/json;charset=UTF-8",
 		"Access-Control-Allow-Origin": "*",
